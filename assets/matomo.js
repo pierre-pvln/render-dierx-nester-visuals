e614a4e0f573fa4de0726bd6024473631112e02f
Pierre Veelen (pvln) <pierre@pvln.nl>
2023-06-28 21:41:29 +0200

    Yet another update at 2023_06_28__21_41_16

--- assets/matomo.js
+++ assets/matomo.js
@@ -16,5 +16,5 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
-<! </script> -->
+<!-- </script> -->
 <!-- End Matomo Code -->
