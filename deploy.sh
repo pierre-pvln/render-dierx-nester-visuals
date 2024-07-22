23dc5269b2ccb702b953872b609e9fb48153a088
Pierre Veelen (pvln) <pierre@pvln.nl>
2024-07-22 21:53:02 +0200

    Yet another update at 2024_07_22__21_52_41

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@
 # @package    dierx-nester-visuals
 #             for Dierx on render
 # 
-# @version    v0.0.1  2022-12-07
+# @version    v0.0.2  2024-07-22
 # @author     pierre@ipheion.eu
 # @copyright  (C) 2022-2022 Pierre Veelen
 #
 # =============================================================================
-/opt/render/project/src/.venv/bin/python3.9 -m pip install --upgrade pip
+#/opt/render/project/src/.venv/bin/python3.9 -m pip install --upgrade pip
+/opt/render/project/src/.venv/bin/python3.11 -m pip install --upgrade pip
 pip install -r requirements.txt
